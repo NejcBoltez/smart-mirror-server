723e49d85d0eaa7972cce1388a86ef13dfa29328
root <nejc.boltez@gmail.com>
2019-06-01 10:12:37 +0100

    first commit

---
+++ SmartMirror.py
@@ -0,0 +1,11 @@
+import time
+import Tkinter as tk
+from Tkinter import *
+
+def ogledalo(self):
+    self.geometry("400x600")
+    self.configure(background='black')
+    
+window=Tk()
+ogledalo(window)
+window.mainloop()
