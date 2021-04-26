31f21be4cdc9c483def38328c03477afcfab9911
NejcBoltez <nejc.boltez@gmail.com>
2021-04-26 19:56:22 +0200

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -14,13 +14,17 @@ sudo ldconfig
 cd ..
 pkg-config --libs --cflags dlib-1
 python setup.py install
-    
+
+#Install python packages
 pip3 install setuptools
 pip3 install opencv-python
+pip3 install pillow
+pip3 install face_recognition
+
+
 sudo apt-get install libatlas-base-dev
 sudo apt-get install libjasper-dev
 sudo apt-get install libqtgui4
 sudo apt-get install python3-pyqt5
 
-pip3 install pillow
-pip3 install face_recognition
+
