9f0fbd0b7c70ceca67df7d6e323e9814190b9c67
NejcBoltez <nejc.boltez@gmail.com>
2021-04-26 19:55:17 +0200

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+#INSTALL DLIB
+sudo apt-get install build-essential cmake pkg-config libx11-dev libatlas-base-dev libgtk-3-dev libboost-python-dev
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+wget http://dlib.net/files/dlib-19.6.tar.bz2
+tar xvf dlib-19.6.tar.bz2
+cd dlib-19.6/
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+sudo make install
+sudo ldconfig
+cd ..
+pkg-config --libs --cflags dlib-1
+python setup.py install
+    
+pip3 install setuptools
+pip3 install opencv-python
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install python3-pyqt5
+
+pip3 install pillow
+pip3 install face_recognition
