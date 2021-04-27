9989c15f9cb86c3753ecc95e251b5ca9b9f00950
NejcBoltez <nejc.boltez@gmail.com>
2021-04-27 20:38:29 +0200

    Update dlib.sh

--- dlib.sh
+++ dlib.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 #INSTALL DLIB
+cd .. #go to previous directory so that we would not have some pushes to git
 sudo apt-get install build-essential cmake pkg-config libx11-dev libatlas-base-dev libgtk-3-dev libboost-python-dev
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 wget http://dlib.net/files/dlib-19.6.tar.bz2
