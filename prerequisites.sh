295b51d83170b632f2e03fc9d2e2531743e560db
NejcBoltez <nejc.boltez@gmail.com>
2021-04-27 21:25:26 +0200

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -40,6 +40,6 @@ sudo apt-get install libatlas-base-dev -y
 sudo apt-get install libjasper-dev -y
 sudo apt-get install libqtgui4 -y
 sudo apt-get install python3-pyqt5 -y
-sudo apt.get install python3-tk -y
+sudo apt-get install python3-tk -y
 
 
