65779f304c14a3e71493687afd5819ed3354be93
NejcBoltez <nejc.boltez@gmail.com>
2022-01-08 16:07:00 +0100

    remove unnecessary prints and comments

--- youtube_stream.py
+++ youtube_stream.py
@@ -1,4 +1,3 @@
-import time
 import vlc
 import pafy
 import sys
@@ -11,7 +10,7 @@ except:
     import Tkinter as tk
     from Tkinter import *
 arguments = list(sys.argv)
-print(arguments)
+
 #Youtube video https://www.youtube.com/watch?v=qmQr0Uyi0Ls
 class Video(Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -27,7 +26,6 @@ class Video(Frame):
         self.video_play=Frame(self.videoframe, background="black", width=1400, height=650)
         self.video_play.pack(fill=BOTH, expand=YES)
         self.url= self.get_URL()
-        #self.url="https://www.youtube.com/watch?v=EUBWcad1U0c"#=xAg7z6u4NE8"
         self.video=pafy.new(self.url) #pip3 install youtube-dl    
         self.best=self.video.getbest()
         self.playurl=self.best.url
@@ -38,19 +36,6 @@ class Video(Frame):
         self.media.get_mrl()
         self.player.set_media(self.media)
         self.player.play()
-        #time.sleep(1)
-        #self.player.pause()
-        #self.start_animation()
-        '''
-    def start_animation(self):
-        if(self.start_w_animation<=self.end_w_animation and self.start_h_animation<=self.end_h_animation):
-            self.video_play.configure(width=self.start_w_animation, height=self.start_h_animation, background="black")
-            self.start_w_animation=self.start_w_animation+10
-            self.start_h_animation=self.start_h_animation+10
-            self.start=self.video_play.after(1, self.start_animation)
-        else:
-            self.video_play.after_cancel(self.start)
-            #self.player.play()'''
 
 
     def get_URL(self):
@@ -60,21 +45,13 @@ class Video(Frame):
         c=0
         selected_url=''
         for i in self.yt:
-            print(c)
-            print(i['url_suffix'])
-            print(i['title'])
             if (c == int(arguments[2])):
-                print(i['url_suffix'])
-                print(i['title'])
                 self.video_name.config(text=str(i['title']))
                 selected_url='https://www.youtube.com'+i['url_suffix']
-                print(selected_url)
             c=c+1
         return selected_url
     def GetHandle(self):
-        print (self.video_play.winfo_id())
         return self.video_play.winfo_id()
-        #time.sleep(50)
 class Window:
     def __init__(self):
         self.tk=tk.Tk()
