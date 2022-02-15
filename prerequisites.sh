9a92b9870778f1653ef81221153c1268639650d5
NejcBoltez <nejc.boltez@gmail.com>
2022-02-15 08:27:19 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -11,6 +11,8 @@ xset q
 
 #Install linux packages
 
+sudo apt-get install build-essential cmake pkg-config libx11-dev libatlas-base-dev libgtk-3-dev libboost-python-dev -y
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y
 sudo apt-get install libatlas-base-dev -y
 sudo apt-get install libjasper-dev -y
 sudo apt-get install libqtgui4 -y
