c434ccc127817b6be0f1173adcdbd25c6a5a7713
NejcBoltez <nejc.boltez@gmail.com>
2022-07-04 22:14:21 +0200

    5 show forms for weather and news on the page (#10)
    * Weather and news form
    
    * create forms for weather and news

--- django_app/forms.py
+++ django_app/forms.py
@@ -1,17 +1,26 @@
 from dataclasses import field
-from .models import Weather
+from .models import Weather, News, APIkeys
+from django import forms
 
-class WeatherForm():
+class WeatherForm(forms.Form):
+    
+    #host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    #topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    country = forms.CharField(max_length=200)
+    city = forms.CharField(max_length=200)
+    coordX = forms.CharField(max_length=200)
+    coordY = forms.CharField(max_length=200)
     class Meta:
         model=Weather
         fields = '__all__'
 
-class WeatherForm():
+class NewsForm():
+    news_to_select = forms.CharField(max_length=200)
     class Meta:
-        model=Weather
+        model=News
         fields = '__all__'
 
 class APIForm():
     class Meta:
-        model=Weather
+        model=APIkeys
         fields = '__all__'
