4dba002b4e7220192e9e267debc78ce8c9cc616c
Nejc Boltez <nejc.boltez@gmail.com>
2022-07-04 21:34:51 +0200

    Weather and news form

--- django_app/views.py
+++ django_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 from .models import Weather
 from .forms import WeatherForm
+
 
 def homePage(request):
     return render(request,'smartmirror_django/home.html')
@@ -9,18 +12,27 @@ def NewsPage(request):
     return render(request,'smartmirror_django/news.html')
 
 def WeatherPage(request):
+    if request.method == 'POST':
 
-    form=WeatherForm()
-    return render(request,'smartmirror_django/weather.html')
+        form=WeatherForm(request.POST)
+        Weather.objects.create(
+            city = request.POST.get('City'),
+            coordX = request.POST.get('coordX'),
+            coordY = request.POST.get('coordY')
+        )
+    else:
+        form=WeatherForm()
+    context = {'form': form}
+    return render(request,'smartmirror_django/weather.html', context)
 def WeatherConfSave(request):
     weatherForm=WeatherForm()
     if request.method == 'POST':
         Weather.objects.create(
-            city = request.POST.get('name'),
-            country = request.POST.get('name'),
-            coordX = "",
-            coordY = "",
-            description=""
+            city = request.POST.get('city'),
+            country = request.POST.get('country'),
+            coordX = request.POST.get('coordX'),
+            coordY = request.POST.get('coordY')
         )
+    return redirect('home', RequestContext(request))
 def UserPage(request):
     return render(request,'smartmirror_django/user_prop.html')
