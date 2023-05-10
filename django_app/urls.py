83b4274962278bd05d5b6aff302b27eb7e348cae
NejcBoltez <nejc.boltez@gmail.com>
2023-05-10 21:50:44 +0200

    Save weather data

--- django_app/urls.py
+++ django_app/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('news', views.NewsPage, name="news"),
     path('weather', views.WeatherPage, name="weather"),
     path('user_profile', views.UserPage, name="user_prop"),
-    path('picture', views.showCamera, name="UserPicture"),
     path('newUser/', views.NewUserPage, name="NewUserPage"),
     path('process_image/', views.process_image, name='process_image'),
 ]
