147ef920556c595ffb87baff298302ab51221cea
NejcBoltez <nejc.boltez@gmail.com>
2023-07-02 17:51:13 +0200

    LOGIN/LOGOUT functionality

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -10,5 +10,7 @@ urlpatterns = [
     path('weather', views.WeatherPage, name="weather"),
     path('user_profile', views.UserPage, name="user_prop"),
     path('newUser/', views.NewUserPage, name="NewUserPage"),
+    path('login', views.Login, name="LoginPage"),
+    path('logout', views.Logout, name="LogoutPage"),
     path('process_image/', views.process_image, name='process_image'),
 ]
