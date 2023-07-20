acda73adc7068a1ab151a7b4cdfee25e325ee6ba
NejcBoltez <nejc.boltez@gmail.com>
2023-07-20 12:00:52 +0200

    Adding views

--- django_app/SmartMirrorViews.py
+++ django_app/SmartMirrorViews.py
@@ -29,16 +29,36 @@ def homePage(request,test):
 	global youtubeData
 	newsData = 1
 	context = { 'data': newsData, 'values':youtubeData}
+	print(test)
 	if request.method == "GET":
 		print(request.path)
 		context['data'] = test
-		if ("weather" in test):
-			context['values'] = Work_with_files.get_yt_data("Iron Man")['videos']
+		if ("home" in test):
+			context['weatherData'] = Work_with_files.read_weather_main()
+			context['newsData'] = Work_with_files.read_news_data()['articles']
+		elif ("weather" in test):
+			context['dates'] = Work_with_files.get_all_dates_for_weather()
+			context['values'] = Work_with_files.read_weather_data()
 		elif ("news" in test):
 			context['values'] = Work_with_files.read_news_data()['articles']
+		elif("wikipedia" in test):
+			try:
+				searchFor = request.META['QUERY_STRING'].replace("&","").split("=")[1]
+				context['values'] = Work_with_files.get_wiki_data(searchFor)
+			except Exception as e:
+				print("ERROR: "+str(e))
+				context['values'] = Work_with_files.get_wiki_data("Bill_gates")
 		elif ("ytSearch" in test):
-			context['values'] = Work_with_files.get_yt_data("Iron Man")['videos']
+			try:
+				searchFor = request.META['QUERY_STRING'].replace("&","").split("=")[1]
+				context['values'] = Work_with_files.get_yt_data(searchFor)['videos']
+			except Exception as e:
+				context['values'] = Work_with_files.getSaved_yt_data()['videos']
 		elif ("ytStream" in test):
-			context['values'] = Work_with_files.stream_youtube(0)
-	providers = ["Google","24ur","rtvslo","siol","racunalniske novice"]
+			try:
+				streamIndex = request.META['QUERY_STRING'].replace("&","").split("=")[1]
+				print(streamIndex)
+				context['values'] = Work_with_files.stream_youtube(int(streamIndex))
+			except Exception as e:
+				context['values'] = Work_with_files.stream_youtube(0)
 	return render(request,'smartmirror_django/SmartMirrorTemplate/home.html',context)
--- django_app/working_with_files.py
+++ django_app/working_with_files.py
@@ -2,6 +2,10 @@ import sys
 import os
 import json
 from youtube_search import YoutubeSearch
+import subprocess
+import sys
+from urllib import request
+from urllib import parse
 
 class Work_with_files:
 	def get_yt_data(search_for):
@@ -15,7 +19,16 @@ class Work_with_files:
 		#print(get_results)
 		return get_results
 
+	def getSaved_yt_data():
+		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+		file_to_open=os.path.join(BASE_DIR, "../json_data"+os.path.sep+"youtube_data.json")
+		get_results=[]
+		with open(file_to_open,"r") as f_r:
+			get_results=json.load(f_r)
+		return get_results
+
 	def stream_youtube(videoToPlay):
+		print("VIDEO INDEX: "+str(videoToPlay))
 		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
 		file_to_open=os.path.join(BASE_DIR, "../json_data"+os.path.sep+"youtube_data.json")
 		with open(file_to_open,"r") as f_r:
@@ -23,6 +36,50 @@ class Work_with_files:
 		print(r_p['videos'][videoToPlay])
 		r_p['videos'][videoToPlay]['videoURL'] = "https://www.youtube.com/embed/" + r_p['videos'][videoToPlay]['id'] + "?autoplay=1"
 		return r_p['videos'][videoToPlay]
+	def save_image(url_of_image):
+		url_split=url_of_image.split(".")
+		file_end=url_split[len(url_split)-1]
+		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+		wiki=os.path.join(BASE_DIR, "./wiki_pictures/wiki.jpg")
+		file_name="./wiki_pictures/wiki."+str(file_end)
+		if(os.path.isfile(file_name)):
+			os.remove(file_name)
+		try:
+			start_popup=subprocess.Popen(["wget", "-O",file_name, url_of_image])
+			start_popup.wait()
+		except Exception as e:
+			print(e)
+	def get_wiki_data(question):
+		wiki_api_url="https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exsentences=6&pithumbsize=500&exintro&explaintext&redirects=1&titles="+str(question.replace("_", "%20"))#str(title[0])
+		print(wiki_api_url)
+		with request.urlopen(wiki_api_url) as wiki_resp:
+			read_wiki=json.loads(wiki_resp.read().decode())
+			#print(read_wiki)
+
+		wiki_response=list(read_wiki["query"]["pages"].values())[0]
+		title=wiki_response["title"]
+		answer=wiki_response["extract"]
+		#print(wiki_response["query"]["pages"].values())
+		try:
+			url_of_image=wiki_response["thumbnail"]["source"]
+		except Exception as e:
+			print(str(e))
+			wiki_response["thumbnail"]={"source":""}
+		print(wiki_response)
+		#save_image(wiki_api_url)
+		'''url_split=url_of_image.split(".")
+		file_end=url_split[len(url_split)-1]
+		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+		wiki=os.path.join(BASE_DIR, "./wiki_pictures/wiki.jpg")
+		file_name="./wiki_pictures/wiki."+str(file_end)
+		if(os.path.isfile(file_name)):
+			os.remove(file_name)
+		try:
+			start_popup=subprocess.Popen(["wget", "-O",file_name, url_of_image])
+			start_popup.wait()
+		except Exception as e:
+			print(e)'''
+		return wiki_response
 	def create_dir_for_user(new_user):
 		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
 		image_dir=os.path.join(BASE_DIR, "../Users")
@@ -83,13 +140,29 @@ class Work_with_files:
 		with open(file_to_open,"r") as f_r:
 			r_p=json.load(f_r)
 		return r_p
-	
+	def get_all_dates_for_weather():
+		r_p=""
+		allDates = []
+		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+		file_to_open=os.path.join(BASE_DIR, "../json_data"+os.path.sep+"forecast_data.json")
+		with open(file_to_open,"r") as f_r:
+			r_p=json.load(f_r)
+		
+		for i in r_p['list']:
+			date = i['dt_txt'].split(" ")[0]
+			if (date not in allDates):
+    				allDates.append(date)
+		return allDates
 	def read_weather_data():
 		r_p=""
 		BASE_DIR= os.path.dirname(os.path.abspath(__file__))
 		file_to_open=os.path.join(BASE_DIR, "../json_data"+os.path.sep+"forecast_data.json")
 		with open(file_to_open,"r") as f_r:
 			r_p=json.load(f_r)
+		
+		for i in r_p['list']:
+			i['date'] = i['dt_txt'].split(" ")[0]
+			print(i)
 		return r_p
 	
 	def read_weather_main():
@@ -98,6 +171,8 @@ class Work_with_files:
 		file_to_open=os.path.join(BASE_DIR, "../json_data"+os.path.sep+"forecast_main.json")
 		with open(file_to_open,"r") as f_r:
 			r_p=json.load(f_r)
+		print(r_p)
+		
 		return r_p
 	
 	def read_weather_today():
